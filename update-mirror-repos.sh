950126a8bf66c5c1141952407de4112de35831b3
shujinwu <shujinwu@oregonstate.edu>
2016-08-09 18:28:46 -0700

    update scripts

--- update-mirror-repos.sh
+++ update-mirror-repos.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
-DEST=git@somewhere:somebody
+# SRC=git@banner-src.ellucian.com:
+SRC=https://github.com
+DEST=git@somewhere:someone
 REMOTE_NAME=ecs
 
 cat src_repos.txt | while read REPO; do
-    REPO_NAME=${REPO##*/}
-    echo $REPO_NAME
-    if [ -d $REPO_NAME ]; then
-        cd $REPO_NAME
-        git fetch -p origin
-        git push --mirror $REMOTE_NAME
-        cd ..
-    else
-        git clone --bare $REPO
-        cd $REPO_NAME
-        git remote add $REMOTE_NAME $DEST/$REPO_NAME
-        git push --mirror $REMOTE_NAME
-        cd ..
+    if [ -n REPO ]; then
+        REPO_NAME=${REPO##*/}.git
+        echo $REPO_NAME
+        if [ -d $REPO_NAME ]; then
+            cd $REPO_NAME
+            # git fetch -p origin
+            # git push --mirror $REMOTE_NAME
+            cd ..
+        else
+            echo $SRC/$REPO.git
+            # git clone --bare $SRC/$REPO
+            # cd $REPO_NAME
+            # git remote add $REMOTE_NAME $DEST/$REPO_NAME
+            # git push --mirror $REMOTE_NAME
+            # cd ..
+        fi
+    fi
+done
+
+cat pages_repos.txt | while read REPO; do
+    if [ -n REPO ]; then
+        REPO_NAME=${REPO##*/}
+        if [ -d $REPO_NAME ]; then
+            cd $REPO_NAME
+            # git fetch -p origin
+            # git push --mirror $REMOTE_NAME
+            cd ..
+        else
+            echo $REPO_NAME
+            # git clone --bare $SRC/$REPO
+            # cd $REPO_NAME
+            # git remote add $REMOTE_NAME $DEST/pages-$REPO_NAME
+            # git push --mirror $REMOTE_NAME
+            # cd ..
+        fi
     fi
 done
